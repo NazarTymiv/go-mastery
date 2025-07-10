e88f9058e5cecf0ad516dc5a468f20d596c182c9
Nazar Tymiv <nazartumiv@gmail.com>
2025-07-10 23:52:10 +0100

    feat safe-bank-transaction: Solution done

--- external-tasks/safe-bank-transaction/main.go
+++ external-tasks/safe-bank-transaction/main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"sync"
 )
-
-var mutex sync.Mutex
 
 type Account struct {
 	name    string
@@ -16,15 +15,21 @@ type Account struct {
 type Transaction struct {
 	from   string
 	to     string
-	amound float64
+	amount float64
 }
 
 type Bank struct {
-	Accounts     []Account
+	Accounts     map[string]*Account
 	Transactions []Transaction
+	mutex        sync.Mutex
 }
 
-func NewBank() Bank
+func NewBank() Bank {
+	return Bank{
+		Accounts:     map[string]*Account{},
+		Transactions: []Transaction{},
+	}
+}
 
 func (b *Bank) CreateAccount(name string, balance float64) {
 	if name == "" {
@@ -32,12 +37,58 @@ func (b *Bank) CreateAccount(name string, balance float64) {
 		return
 	}
 
-	b.Accounts = append(b.Accounts, Account{name: name, balance: balance})
+	b.Accounts[name] = &Account{
+		name:    name,
+		balance: balance,
+	}
 
 	log.Printf("Account %s created successfully with balance £%.2f", name, balance)
 }
 
+// Make transaction
 func (b *Bank) Transfer(from string, to string, amount float64) error {
+	b.mutex.Lock()
+	defer b.mutex.Unlock()
+
+	sender := b.Accounts[from]
+	receiver := b.Accounts[to]
+	if sender == nil || receiver == nil {
+		return errors.New("Account not found")
+	}
+
+	if sender.balance < amount {
+		return errors.New("Sender does not have enough money to send")
+	}
+
+	sender.balance -= amount
+	receiver.balance += amount
+
+	b.Transactions = append(b.Transactions, Transaction{
+		to:     to,
+		from:   from,
+		amount: amount,
+	})
+
+	return nil
+}
+
+// Get balance by username
+func (b *Bank) GetBalance(name string) float64 {
+	account := b.Accounts[name]
+	if account == nil {
+		log.Printf("Account %s not found", name)
+		return 0
+	}
+	return account.balance
+}
+
+// PrintTransactions
+func (b *Bank) PrintTransactions() {
+	fmt.Println("Transactions List:")
+
+	for i, t := range b.Transactions {
+		fmt.Printf("%d. %s transferred £%.2f to %s\n", i+1, t.from, t.amount, t.to)
+	}
 }
 
 func main() {
@@ -48,12 +99,23 @@ func main() {
 
 	err := bank.Transfer("user1", "user2", 30)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+	}
+	err = bank.Transfer("user1", "user2", 30)
+	if err != nil {
+		log.Println(err)
+	}
+	err = bank.Transfer("user1", "user2", 30)
+	if err != nil {
+		log.Println(err)
+	}
+	err = bank.Transfer("user1", "user2", 30)
+	if err != nil {
+		log.Println(err)
 	}
 
 	fmt.Println(bank.GetBalance("user1")) // 70
 	fmt.Println(bank.GetBalance("user2")) // 80
 
 	bank.PrintTransactions()
-
 }
