6cc3981e84f7ae1f6af5632b83c3497f6223800c
Nazar Tymiv <nazartumiv@gmail.com>
2025-07-29 00:02:31 +0100

    feat Day-15: created /login route with sessiontoken and csrftoken

--- Week-3/Day-15-middleware-auth/handlers/auth/login.go
+++ Week-3/Day-15-middleware-auth/handlers/auth/login.go
@@ -1,7 +1,46 @@
 package auth
 
-import "net/http"
+import (
+	"net/http"
+	"time"
+
+	"github.com/nazartymiv/go-mastery/Week-3/Day-15-middleware/db"
+	"github.com/nazartymiv/go-mastery/Week-3/Day-15-middleware/helpers"
+	"github.com/nazartymiv/go-mastery/Week-3/Day-15-middleware/logger"
+	"github.com/nazartymiv/go-mastery/Week-3/Day-15-middleware/utils"
+)
 
 func Login(w http.ResponseWriter, r *http.Request) {
+	username := r.FormValue("username")
+	password := r.FormValue("password")
 
+	user, ok := db.Users[username]
+	if !ok || !utils.CheckPasswordHash(password, user.HashedPassword) {
+		helpers.SendError(w, "invalid username/password", http.StatusUnauthorized)
+		logger.Error("[Login Handler]: Invalid credentials", map[string]string{"username": username, "password": password})
+		return
+	}
+
+	sessionToken := utils.GenerateToken(32)
+	csrfToken := utils.GenerateToken(32)
+
+	http.SetCookie(w, &http.Cookie{
+		Name:     "session_token",
+		Value:    sessionToken,
+		Expires:  time.Now().Add(24 * time.Hour),
+		HttpOnly: true,
+	})
+
+	http.SetCookie(w, &http.Cookie{
+		Name:     "csrf_token",
+		Value:    csrfToken,
+		Expires:  time.Now().Add(24 * time.Hour),
+		HttpOnly: false,
+	})
+
+	user.SessionToken = sessionToken
+	user.CSRFToken = csrfToken
+	db.Users[username] = user
+
+	helpers.SendSuccess(w, helpers.MessageResponse{Message: "Login successful!"}, http.StatusOK)
 }
--- Week-3/Day-15-middleware-auth/utils/utils.go
+++ Week-3/Day-15-middleware-auth/utils/utils.go
@@ -1,8 +1,29 @@
 package utils
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"log"
+
+	"github.com/nazartymiv/go-mastery/Week-3/Day-15-middleware/logger"
+	"golang.org/x/crypto/bcrypt"
+)
 
 func HashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 10)
 	return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
+func GenerateToken(length int) string {
+	bytes := make([]byte, length)
+	if _, err := rand.Read(bytes); err != nil {
+		logger.Error("Failed to generate token", err.Error())
+		log.Fatal()
+	}
+	return base64.URLEncoding.EncodeToString(bytes)
 }
